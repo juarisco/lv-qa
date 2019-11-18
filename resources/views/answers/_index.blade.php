d96e54810e344be6b09352b2adb26ad98880e110
Josef <juarisco@outlook.com>
2019-11-18 16:17:09 -0500

    Lesson-37.-Accepting-the-answer-as-best-answer-Part-1-of-2

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -18,9 +18,15 @@
                             <a href="" title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a href="" title="Mark this answer as best answer" class="{{ $answer->status }} mt-2">
+                            <a href="" title="Mark this answer as best answer" class="{{ $answer->status }} mt-2"
+                                onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();">
                                 <i class="fas fa-check fa-2x"></i>
                             </a>
+                            <form id="accept-answer-{{ $answer->id }}"
+                                action="{{ route('answers.accept', $answer->id) }}" method="post"
+                                style="display: none;">
+                                @csrf
+                            </form>
                         </div>
 
                         <div class="media-body">
