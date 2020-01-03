7a799d117eae417489362e4d4d6bf3e5eca20129
Josef <juarisco@outlook.com>
2020-01-03 17:04:17 -0500

    Lesson-51.-Refactoring-The-Views-Part-2-of-2

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,79 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-title">
-                        <div class="d-flex align-items-center">
-                            <h1>{{ $question->title }}</h1>
-                            <div class="ml-auto">
-                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">
-                                    Back to all Questions
-                                </a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h1>{{ $question->title }}</h1>
+                                <div class="ml-auto">
+                                    <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">
+                                        Back to all Questions
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <hr>
-                    <div class="media">
-                        <div class="d-flex flex-column vote-controls">
-                            <a href="" title="This question is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                                onclick="event.preventDefault(); document.getElementById('up-vote-question-{{ $question->id }}').submit();">
-                                <i class="fas fa-caret-up fa-3x"></i>
-                            </a>
-                            <form id="up-vote-question-{{ $question->id }}" action="/questions/{{ $question->id }}/vote"
-                                method="post" style="display: none;">
-                                @csrf
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-                            <span class="votes-count">{{ $question->votes_count }}</span>
-                            <a href="" title="This question is not useful"
-                                class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                                onclick="event.preventDefault(); document.getElementById('down-vote-question-{{ $question->id }}').submit();">
-                                <i class="fas fa-caret-down fa-3x"></i>
-                            </a>
-                            <form id="down-vote-question-{{ $question->id }}"
-                                action="/questions/{{ $question->id }}/vote" method="post" style="display: none;">
-                                @csrf
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-                            <a href="" title="Click to mark as favorite question (Click again to undo)"
-                                class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
-                                onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();">
-                                <i class="fas fa-star fa-2x"></i>
-                                <span class="favorites-count">{{ $question->favorites_count }}</span>
-                            </a>
-                            <form id="favorite-question-{{ $question->id }}"
-                                action="/questions/{{ $question->id }}/favorites" method="post" style="display: none;">
-                                @csrf @if ($question->is_favorited) @method('DELETE') @endif
-                            </form>
-                        </div>
-                        <div class="media-body">
-                            {!! $question->body_html !!}
-                            <div class="row">
-                                <div class="col-4"></div>
-                                <div class="col-4"></div>
-                                <div class="col-4">
-                                    @include('shared._author', [
-                                    'model' => $question,
-                                    'label' => 'Asked'
-                                    ])
+                        <hr>
+                        <div class="media">
+
+                            @include('shared._vote', [
+                                'model' => $question
+                            ])
+
+                            <div class="media-body">
+                                {!! $question->body_html !!}
+                                <div class="row">
+                                    <div class="col-4"></div>
+                                    <div class="col-4"></div>
+                                    <div class="col-4">
+                                        @include('shared._author', [
+                                        'model' => $question,
+                                        'label' => 'Asked'
+                                        ])
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
 
-    @include('answers._index', [
-    'answers' => $question->answers,
-    'answerCount' => $question->answers_count
-    ])
-    @include('answers._create')
-</div>
+        @include('answers._index', [
+        'answers' => $question->answers,
+        'answerCount' => $question->answers_count
+        ])
+        @include('answers._create')
+    </div>
 @endsection
