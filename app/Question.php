d3713b300107c8ebffc851394c0aa4bad7d99e4e
Josef <juarisco@outlook.com>
2020-01-04 10:28:57 -0500

    Lesson-52.-Preventing-The-Application-from-XSS-Attack-Part-1-of-2

--- app/Question.php
+++ app/Question.php
@@ -48,7 +48,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return $this->bodyHtml();
     }
 
     public function answers()
@@ -80,5 +80,20 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
+    }
+
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length)
+    {
+        return str_limit(strip_tags($this->bodyHtml()), $length);
+    }
+
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
